d4f5ca73a3f869c925d378343d441ed808b8fdb4
besvova490 <besvova490@gmail.com>
2021-09-19 11:21:10 +0300

    added cors origins

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 
 //routes
 const authRouter = require("./routes/auth");
@@ -10,6 +11,10 @@ require("dotenv").config()
 
 const app = express();
 
+
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/auth", authRouter);
